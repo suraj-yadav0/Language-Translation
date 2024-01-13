a034a90318c00482c1e985c4a2caf5ccd3d78893
Suraj Yadav <90672206+suraj-yadav0@users.noreply.github.com>
2024-01-13 14:55:49 +0530

    Completed the Language Translator app.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import*
 from tkinter import ttk
+from tkinter import END
 from googletrans import Translator, LANGUAGES
 
 root = Tk()
@@ -15,5 +16,24 @@ Label(root, text="Enter Text", font='arial 13 bold' , bg='white smoke').place(x=
 Input_text = Entry(root, width=60)
 Input_text.place(x=30,y=130)
 Input_text.get()
+
+Label(root,text="Output", font='arial 13 bold',  bg='white smoke').place(x=780,y = 90)
+Output_text = Text(root, font='arial 10' , height=5 , wrap= WORD , pady=5,padx=5,width=50)
+Output_text.place(x=600, y = 130)
+
+language = list(LANGUAGES.values())
+
+dest_lang = ttk.Combobox(root, values=language, width=22)
+dest_lang.place(x = 130 , y = 180)
+dest_lang.set('choose language')
+
+def Translate():
+    translator = Translator()
+    translated = translator.translate(text=Input_text.get(), dest=dest_lang.get())
+    Output_text.delete(1.0, END)
+    Output_text.insert(END , translated.text)
+    
+trans_btn = Button(root, text='Translate',font='arial 12 bold' , pady=5,command= Translate , bg='orange' , activebackground= 'green')
+trans_btn.place(x=445, y = 180)  
 
 root.mainloop()
